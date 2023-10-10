6884c3802a4a89840e129c47df577b96301d6ed5
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2023-10-10 16:59:39 +0200

    update2

---
+++ cpp vsc/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int x = 2;
+int y = 5;
+
+int main(){
+
+    cout << x + y;
+    getchar();
+    return 0;
+
+}
---
+++ cpp vsc/test1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main(){
+
+int gowno = 2137;
+
+
+std::cout<<"SRAKA \n";
+std::cout<<gowno;
+
+}
