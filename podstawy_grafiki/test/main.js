825af6f50f3efd5ab314abc977c050fa34c830d1
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2023-10-10 20:11:59 +0200

    asdfg

--- podstawy_grafiki/test/main.js
+++ podstawy_grafiki/test/main.js
@@ -7,7 +7,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+scene.background = new THREE.Color(0xffffff);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -16,18 +16,48 @@ document.body.appendChild( renderer.domElement );
 const controls = new OrbitControls( camera, renderer.domElement );
 const loader = new GLTFLoader();
 
-const geometry = new THREE.CircleGeometry(5);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const geometry = new THREE.BoxGeometry(2,2,2);
+const material = new THREE.MeshBasicMaterial( { color: 0xaaffdd } );
+
+const sciany = [
+	new THREE.MeshBasicMaterial({ color: 0xff0000 }), // Czerwony
+	new THREE.MeshBasicMaterial({ color: 0x00ff00 }), // Zielony
+	new THREE.MeshBasicMaterial({ color: 0x0000ff }), // Niebieski
+	new THREE.MeshBasicMaterial({ color: 0xffff00 }), // Żółty
+	new THREE.MeshBasicMaterial({ color: 0xff00ff }), // Magenta
+	new THREE.MeshBasicMaterial({ color: 0x00ffff })  // Cyjan
+  ];
+const kostki = [];
+
 const cube = new THREE.Mesh( geometry, material );
+cube.translateX(0);
 scene.add( cube );
 
-camera.position.z = 100;
+for(var i = 0; i < 16;i+=5){
+	for(var j = 0; j < 16;j+=5){
+		for(var k = 0; k < 16;k+=5){
+	var mesh  = new THREE.Mesh(geometry, sciany);
+	mesh.translateX(i);
+	mesh.translateY(j);
+	mesh.translateZ(k);
+	kostki.push(mesh);
+	scene.add(mesh);
+}}}
+
+
+camera.position.z = 60;
+
 
 function animate() {
 	requestAnimationFrame( animate );
 
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	for (const mesh of kostki) {
+		// Obrót wokół osi Y o 0.01 radianów w każdej klatce animacji
+		mesh.rotation.y += 0.01;
+	  
+		// Obrót wokół osi X o 0.01 radianów w każdej klatce animacji
+		mesh.rotation.x += 0.01;
+	}
 
 	renderer.render( scene, camera );
 }
